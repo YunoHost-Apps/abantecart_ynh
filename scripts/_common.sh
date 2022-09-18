cec1cda92466ae67ff7fba5581b2e6383462758a
yalh76 <yalh@yahoo.com>
2022-09-18 04:58:08 +0200

    Apply example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+php_dependencies="php$YNH_DEFAULT_PHP_VERSION-iconv php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-mysqli php$YNH_DEFAULT_PHP_VERSION-pdo_mysql php$YNH_DEFAULT_PHP_VERSION-zip php$YNH_DEFAULT_PHP_VERSION-soap"
+
+# dependencies used by the app (must be on a single line)
+pkg_dependencies="curl wget git libwebp-dev libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng-dev libxpm-dev libonig-dev libzip-dev mariadb-server mc net-tools libxml2-dev $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
